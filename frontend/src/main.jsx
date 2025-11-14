af5e5543150f66518505e88e5e880c1db7119fb7
Mamta Singh <mamta252002singh@gmail.com>
2025-11-14 11:59:07 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
-  export const serverUrl="http://localhost:8000"
+  export const serverUrl="https://real-time-chat-app-backend-qylm.onrender.com"
 
 createRoot(document.getElementById('root')).render(
  <BrowserRouter >
